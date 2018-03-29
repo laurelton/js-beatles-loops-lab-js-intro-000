e7c71c7f261172386b9014e7e8cedb9a04afb8d1
Frank Rose <frankrosework@gmail.com>
2018-03-29 04:34:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var theBeatlesPlay = function(musicians, instruments) {
   var plays = [];
   for (let i = 0; i < musicians.length; i++) {
-    plays = musicians[i] + ' plays ' + instruments[i];
+    plays.push(musicians[i] + ' plays ' + instruments[i]);
   }
   return plays;
 };
